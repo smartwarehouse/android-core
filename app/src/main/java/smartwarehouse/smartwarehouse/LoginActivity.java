29032fb0e5c2434d8605e82114b153d1acda69d2
Kiekiek <rifkidwi35@yahoo.co.id>
2018-09-24 10:28:09 +0700

    1.0.0

--- app/src/main/java/smartwarehouse/smartwarehouse/LoginActivity.java
+++ app/src/main/java/smartwarehouse/smartwarehouse/LoginActivity.java
@@ -1,7 +1,10 @@
 package smartwarehouse.smartwarehouse;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "Montserrat-Medium.tff");
+        ((EditText)findViewById(R.id.namaPengguna)).setTypeface(font);
+        ((EditText)findViewById(R.id.password)).setTypeface(font);
     }
 }
