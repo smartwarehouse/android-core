03a5e8ab94994019e30af23a1dc13fcfe639cc63
Kiekiek <rifkidwi35@yahoo.co.id>
2018-10-05 21:38:27 +0700

    first commit

--- app/src/main/java/smartwarehouse/smartwarehouse/BarangKosong.java
+++ app/src/main/java/smartwarehouse/smartwarehouse/BarangKosong.java
@@ -3,11 +3,11 @@ package smartwarehouse.smartwarehouse;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Coba extends AppCompatActivity {
+public class BarangKosong extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_coba);
+        setContentView(R.layout.activity_barang_kosong);
     }
 }
